159eb983d6877216e23721586cc5f4b1fc85d3f8
wqsudotee <p4cket@protonmail.com>
2018-04-08 13:37:28 +1200

    adding these little games

---
+++ tetris/src/consts.js
@@ -0,0 +1,67 @@
+
+//colors for shapes
+var colors = ['#00af9d','#ffb652','#cd66cc','#66bc29','#0096db','#3a7dda','#ffe100'];
+
+//sidebar width
+var sideWidth = 120;
+
+
+//scene column count
+var columnCount = 10;
+
+//scene row count;
+var rowCount = 20;
+
+//previewCount
+var previewCount = 6;
+
+//scene gradient start color 
+var sceneBgStart = '#CACACC';
+
+//scene gradient end color 
+var sceneBgEnd = '#CACACC';
+
+//preview background color
+var previewBg = '#2f2f2f';
+
+//grid line color
+var gridLineColor = '#373f54';
+
+//box border color
+var boxBorderColor = '#373f54';
+
+
+// Game speed
+var defaultInterval = 600;
+
+
+// Level update interval 
+var levelInterval = 120 * 1000; 
+
+
+
+var exports = module.exports = {};
+
+exports.COLORS =  colors;
+
+exports.SIDE_WIDTH = sideWidth;
+
+exports.ROW_COUNT = rowCount;
+
+exports.COLUMN_COUNT = columnCount;
+
+exports.SCENE_BG_START = sceneBgStart;
+
+exports.SCENE_BG_END = sceneBgEnd;
+
+exports.PREVIEW_BG = previewBg;
+
+exports.PREVIEW_COUNT = previewCount;
+
+exports.GRID_LINE_COLOR = gridLineColor;
+
+exports.BOX_BORDER_COLOR = boxBorderColor;
+
+exports.DEFAULT_INTERVAL = defaultInterval;
+
+exports.LEVEL_INTERVAL = levelInterval;
